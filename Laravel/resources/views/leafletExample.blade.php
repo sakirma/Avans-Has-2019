f554bc90d34442a7aa4d169c1bcbd9efe0a77bc0
Huseyin Caliskan <huseyin.32@live.nl>
2019-02-17 21:26:29 +0100

    Working map

--- Laravel/resources/views/leafletExample.blade.php
+++ Laravel/resources/views/leafletExample.blade.php
@@ -7,8 +7,23 @@
     <title>Leaflet</title>
 </head>
 
+
+{!! HTML::script('js/leaflet.js')!!}
 {!! HTML::style('css/leaflet.css') !!}
+{!! HTML::style('css/example-css/leaflet-map.css') !!}
+
 <body>
-    
+    <div id="mapid"> </div>
+
+    <script> 
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox.streets',
+            accessToken: 'pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig'
+        }).addTo(mymap);
+    </script>
 </body>
 </html>
