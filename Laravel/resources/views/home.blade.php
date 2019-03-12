401bebf7c96da92d08c828ce4d5b1e5a2c0e4b21
Huseyin Caliskan <huseyin.32@live.nl>
2019-03-12 23:44:15 +0100

    potential fix for map loading

--- Laravel/resources/views/home.blade.php
+++ Laravel/resources/views/home.blade.php
@@ -49,13 +49,10 @@
     <script>
         var mymap = L.map('mapid').setView([52.143438, 6.215206], 7.5);
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/sakirma/cjsj37dym6e401gqi2zcu2aph/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig', {
+        L.tileLayer('https://api.mapbox.com/styles/v1/sakirma/cjsj37dym6e401gqi2zcu2aph/tiles/256/{z}/{x}/{y}@2x?fresh=true&access_token=pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
             maxZoom: 18,
-            id: 'sakirma.cjsj37dym6e401gqi2zcu2aph',
-            accessToken: 'pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig' // AccessToken has only read permission.
         }).addTo(mymap);
-
 
         // TODO: This has to be done through the database. Retrieve the GeoJSON data and convert it using json_encode().
         L.geoJSON(example).addTo(mymap);
