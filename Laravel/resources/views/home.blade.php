997236422c4ecd4e23b40a8cc56f57ba40dec5ae
Storm <stormwitziers123@gmail.com>
2019-03-12 10:48:38 +0100

    updated containers

--- Laravel/resources/views/home.blade.php
+++ Laravel/resources/views/home.blade.php
@@ -15,21 +15,23 @@
     <title>RoadLeaf</title>
 </head>
 <body>
-    <div id="mysidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="{{ url('/home')}}"> Home</a>
-        <a href="{{ url('/snuffelen')}}"> Snuffelen</a>
-        <a href="{{ url('/about')}}"> About</a>
-    </div>
-    <span class="navbtn" style="font-size:30px;cursor:pointer;" onclick="openNav()">&#9776;</span>
-    <div id="container">
-        <img id="background-image" src="{{ asset('img/main.jpg') }}" alt="Bossen van Den Bosch">
-        <div id="banner">
-            <p>Welkom bij<p>
-            <h1>Natuurlijke Brabantse schoonheid</h1>
-            <p>Scroll naar beneden om</p>
-            <p>Brabant te beleven...</p>
+    <div class="container">
+        <div id="mysidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="{{ url('/home')}}"> Home</a>
+            <a href="{{ url('/snuffelen')}}"> Snuffelen</a>
+            <a href="{{ url('/about')}}"> About</a>
         </div>
-    </div>    
+        <span class="navbtn" style="font-size:30px;cursor:pointer;" onclick="openNav()">&#9776;</span>
+        <div id="container">
+            <img id="background-image" src="{{ asset('img/main.jpg') }}" alt="Bossen van Den Bosch">
+            <div id="banner">
+                <p>Welkom bij<p>
+                <h1>Natuurlijke Brabantse schoonheid</h1>
+                <p>Scroll naar beneden om</p>
+                <p>Brabant te beleven...</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
