b28b10cdd9b04623e1545d5f0f37caae007c0271
Storm <stormwitziers123@gmail.com>
2019-03-13 20:28:54 +0100

    added grouping catagory function for the map

--- Laravel/resources/views/home.blade.php
+++ Laravel/resources/views/home.blade.php
@@ -43,26 +43,11 @@
         <div id="leaflet"></div>
         <div id="legenda">
             @foreach($categories as $category)
-                <button type="button">{{ $category->name }}</button> 
+                <button type="button" onclick="changeToCategory( '{{$category->name}}' )">{{ $category->name }}</button> 
             @endforeach
         </div>
     </div>    
-        <script>
-            var mymap = L.map('leaflet').setView([52.143438, 6.215206], 7.5);
-
-            L.tileLayer('https://api.mapbox.com/styles/v1/sakirma/cjsj37dym6e401gqi2zcu2aph/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig', {
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                maxZoom: 18,
-                id: 'sakirma.cjsj37dym6e401gqi2zcu2aph',
-                accessToken: 'pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig' // AccessToken has only read permission.
-            }).addTo(mymap);
-
-
-            // TODO: This has to be done through the database. Retrieve the GeoJSON data and convert it using json_encode().
-            L.geoJSON(example).addTo(mymap);
-
-            console.log(example);
-        </script>
+        <script type="text/javascript" src="{{ asset('js/leafletmap.js') }}"></script>
     </div>
 </body>
 </html>
