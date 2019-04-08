7f3a5e06c5cf8562d52ac05ae33b4f3872976a90
randyAnft <ranft@avans.nl>
2019-04-09 00:01:29 +0200

    formatting

--- Laravel/resources/views/routes/routeList.blade.php
+++ Laravel/resources/views/routes/routeList.blade.php
@@ -2,13 +2,9 @@
 <html>
 
 <head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/project.css') }}">
-
     <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
-    
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script type="text/javascript" src="{{ asset('js/navbar.js') }}"></script>
 </head>
@@ -21,15 +17,13 @@
         <a href="{{ url('/project')}}"> Project</a>
     </div>
     <span class="navbtn" style="font-size:30px;cursor:pointer;" onclick="openNav()">&#9776;</span>
-
-
-
-
     <div class="container">
         <div id="banner">
             <img src="{{ asset('img/about-banner2.jpg') }}" alt="den bosch bossen">
             <h1>Routes</h1>
         </div>
+
+        <!-- fetched css from the project page and combined it with bootstraps --> 
         <div id="project-exp">
             @foreach ($routes as $route)
             <a class="btn btn-success" href="{{url('/home')}}/#map-container" role="button">{{ $route->name }} {{ $route->length }}</a>
