46562b70232377a4be101ebb41b12cf9ad77841e
HuseyinCaliskan <huseyincaliskan32@gmail.com>
2019-04-08 13:05:58 +0200

    Added migration support, changed gitignore for projec

---
+++ Laravel/database/migrations/2019_04_05_094455_create_project_category_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_category', function (Blueprint $table) {
+            $table->string('name')->primary();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_category');
+    }
+}
