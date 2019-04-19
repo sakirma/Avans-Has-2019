f836558da6c97a0170f08694bab2c4dcf19d1733
Storm <stormwitziers123@gmail.com>
2019-04-19 18:11:22 +0200

    Placed markers get their rout calculated
    TODO: remove placed ROUTE markers the normal ones get removed.
    TODO: update the array when ROUTE markers get moved

--- Laravel/public/js/leaflet_create.js
+++ Laravel/public/js/leaflet_create.js
@@ -1,15 +1,42 @@
-import {init} from './leaflet_init'
+let placedMarkers = L.featureGroup().addTo(map);
+let routingControl;
 
-let placedMarkers = [];
+addEventListener('core_finished', function (e) {
+    console.log('Leaflet core is finished loading');
 
-document.addEventListener("DOMContentLoaded", function(event){
+    routingControl = L.Routing.control({
+        router: L.Routing.mapbox('pk.eyJ1Ijoic2FraXJtYSIsImEiOiJjanM5Y3kzYm0xZzdiNDNybmZueG5jeGw0In0.yNltTMF52t5uEFdU15Uxig'),
+        waypoints: [null],
+        routeWhileDragging: true
+    }).addTo(map);
 
-    init();
-    console.log('Leaflet is initialized!');
-
+    map.doubleClickZoom.disable();
 });
 
-
-function placeMarker(){
-
+function placeMarker(e) {
+    L.marker(e.latlng).addTo(placedMarkers);
+    if (placedMarkers.getLayers().length > 1) {
+        calculateRoute();
+    }
 }
+
+//TODO rmove the maker
+//TODO fix bug where markers move back to initial position when moved.
+function removeMarker(e) {
+    map.removeLayer(e.layer);
+}
+
+function calculateRoute() {
+
+    let layers = placedMarkers.getLayers();
+    let latlngs = [];
+
+    for (let i = 0; i < layers.length; i++) {
+        latlngs.push(L.latLng(layers[i].getLatLng().lat, layers[i].getLatLng().lng));
+    }
+
+    routingControl.setWaypoints(latlngs);
+}
+
+map.on('dblclick', placeMarker);
+placedMarkers.on('dblclick', removeMarker);
--- Laravel/resources/views/createRoute.blade.php
+++ Laravel/resources/views/createRoute.blade.php
@@ -10,11 +10,26 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+
+    {!! HTML::style('css/leaflet/leaflet.css') !!}
+    {!! HTML::style('css/leaflet/leaflet-map.css') !!}
+    {!! HTML::style('css/leaflet/leaflet-routing-machine.css') !!}
+
+    {!! HTML::script('js/leaflet.js')!!}
+    {!! HTML::script('js/leaflet/leaflet-routing-machine.js')!!}
+
+
+
 </head>
 
 <body>
 
-<script type="text/javascript" src="{{ asset('js/leaflet_create.js') }}"></script>
+<div id="leaflet" style="height: 100vh">
+    <script type="text/javascript" src="{{ asset('js/leaflet_core.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/leaflet_create.js') }}"></script>
+</div>
+
+
 
 </body>
 </html>
