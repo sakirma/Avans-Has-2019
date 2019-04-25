b0c8b36361870cd2ca0dc0f6bbf1d3889576cb5f
HuseyinCaliskan <huseyincaliskan32@gmail.com>
2019-04-25 23:52:10 +0200

    Added button

--- Laravel/resources/js/components/ProjectPage/ProjectPageHeader.vue
+++ Laravel/resources/js/components/ProjectPage/ProjectPageHeader.vue
@@ -1,17 +1,23 @@
 <template>
-    <v-layout align-center justify-center row fill-height>
+    <v-layout align-center justify-center row fill-height ml-3>
         <v-flex shrink class="hidden-sm-and-down v-card--reveal">
-            <v-img src="img/RoadLeaf-Logo.png"
-                   height="100%"
-                   max-height="100px"
-                   contain
-                   style="filter: brightness(0) invert(1);"
-                   width="150px"/>
+            <v-layout align-center justify-center fill-height>
+                <v-img src="img/RoadLeaf-Logo.png"
+                       height="100%"
+                       max-height="100px"
+                       contain
+                       style="filter: brightness(0) invert(1);"
+                       width="150px"/>
+                <v-btn fab dark class="ml-5">
+                    <v-icon>
+                        arrow_upward
+                    </v-icon>
+                </v-btn>
+            </v-layout>
         </v-flex>
-        <v-spacer></v-spacer>
-        <v-btn>
-            WOW
-        </v-btn>
+        <v-flex grow>
+
+        </v-flex>
     </v-layout>
 </template>
 
