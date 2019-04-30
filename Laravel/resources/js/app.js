a748c0298dd16be13a0b8d40ec830446f0fe2c7a
randyAnft <ranft@avans.nl>
2019-04-30 12:40:17 +0200

    dsfafds

--- Laravel/resources/js/app.js
+++ Laravel/resources/js/app.js
@@ -64,7 +64,7 @@ Vue.component('map-page',
 );
 
 Vue.component('mark-project',
-    require('./components/MarkProject').default
+    require('./components/AddProject').default
 );
 
 Vue.component('view-projects',
