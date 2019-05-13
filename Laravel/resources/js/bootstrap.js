26f0ebb4b97ca44b28c126e0df2fc1d7d0c52ebd
Huseyin Caliskan <huseyin.32@live.nl>
2019-05-13 13:38:48 +0200

    Changed baseurl back for development

--- Laravel/resources/js/bootstrap.js
+++ Laravel/resources/js/bootstrap.js
@@ -21,7 +21,7 @@ try {
  */
 
 window.axios = require('axios');
-window.axios.defaults.baseURL = "http://www.roadleaf.nl/";
+window.axios.defaults.baseURL = "http://127.0.0.1:8000/";
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
