02368efb3b3650ceb30165f698f857c1df23ef25
Huseyin Caliskan <huseyin.32@live.nl>
2019-05-23 16:22:41 +0200

    Working on Projecten page table view

--- Laravel/resources/js/components/admin/MainCrudPage.vue
+++ Laravel/resources/js/components/admin/MainCrudPage.vue
@@ -6,31 +6,16 @@
                     <c-r-u-d-page-header></c-r-u-d-page-header>
                 </v-flex>
                 <v-flex d-flex style="background-color: white;">
-                    <v-container fluid fill-height pa-0>
+                    <v-container fluid fill-height pt-3 pb-5>
                         <v-layout row fill-height>
                             <v-flex xs6 d-flex px-4>
                                 <v-layout column fill-height>
                                     <v-flex xs1>
-                                        <v-layout row fill-height align-center justify-center>
-                                            <v-flex xs1>
-                                                <v-card-title class="display-2" style="color: #89a324;">
-                                                    Projecten
-                                                </v-card-title>
-                                            </v-flex>
-                                            <v-flex fill-height>
-                                                <v-layout row fill-height align-center justify-end>
-                                                    <v-flex xs2 align-self-center mx-3>
-                                                        <v-btn block style="height: 100%;" color="#89a324">
-                                                            <v-card style="white-space: normal;" color="transparent" flat class="white--text">
-                                                                Project Toevoegen
-                                                            </v-card>
-                                                        </v-btn>
-                                                    </v-flex>
-                                                    <v-flex xs5>
-                                                        <v-text-field solo hide-details round label="zoek een project" class="search-bar pa-0" flat append-icon="search"></v-text-field>
-                                                    </v-flex>
-                                                </v-layout>
-                                            </v-flex>
+                                        <projects-header></projects-header>
+                                    </v-flex>
+                                    <v-flex d-flex style="background-color: red">
+                                        <v-layout >
+
                                         </v-layout>
                                     </v-flex>
                                 </v-layout>
@@ -50,11 +35,12 @@
 
 <script>
     import CRUDPageHeader from './CRUDPageHeader';
-
+    import ProjectsHeader from './headers/ProjectsHeader'
     export default {
         name: "MainCrudPage",
         components: {
-            CRUDPageHeader
+            CRUDPageHeader,
+            ProjectsHeader
         }
     }
 </script>
---
+++ Laravel/resources/js/components/admin/headers/ProjectsHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-layout row fill-height align-center justify-center>
+        <v-flex xs1>
+            <v-card-title class="display-2" style="color: #89a324;">
+                Projecten
+            </v-card-title>
+        </v-flex>
+        <v-flex fill-height>
+            <v-layout row fill-height align-center justify-end>
+                <v-flex xs2 align-self-center mx-3>
+                    <v-btn block style="height: 100%;" color="#89a324">
+                        <v-card style="white-space: normal;" color="transparent" flat class="white--text">
+                            Project Toevoegen
+                        </v-card>
+                    </v-btn>
+                </v-flex>
+                <v-flex xs5>
+                    <v-text-field solo hide-details round label="zoek een project" class="search-bar pa-0" flat append-icon="search"></v-text-field>
+                </v-flex>
+            </v-layout>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "ProjectsHeader"
+    }
+</script>
+
+<style scoped>
+
+</style>
