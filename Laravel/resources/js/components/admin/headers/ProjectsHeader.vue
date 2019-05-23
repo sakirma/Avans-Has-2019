faa74def189f1a034316f8fc8417259887cf4a90
Huseyin Caliskan <huseyin.32@live.nl>
2019-05-23 17:59:37 +0200

    max width of button is assigned

--- Laravel/resources/js/components/admin/headers/ProjectsHeader.vue
+++ Laravel/resources/js/components/admin/headers/ProjectsHeader.vue
@@ -1,22 +1,18 @@
 <template>
     <v-layout row fill-height align-center justify-center>
-        <v-flex xs1>
+        <v-flex d-flex>
             <v-card-title class="display-2" style="color: #89a324;">
                 Projecten
             </v-card-title>
         </v-flex>
-        <v-flex fill-height>
+        <v-flex fill-height xs6>
             <v-layout row fill-height align-center justify-end>
-                <v-flex xs2 align-self-center mx-3>
-                    <v-btn block style="height: 100%;" color="#89a324">
-                        <v-card style="white-space: normal;" color="transparent" flat class="white--text">
-                            Project Toevoegen
-                        </v-card>
-                    </v-btn>
-                </v-flex>
-                <v-flex xs5>
-                    <v-text-field solo hide-details round label="zoek een project" class="search-bar pa-0" flat append-icon="search"></v-text-field>
-                </v-flex>
+                <v-btn style="max-width: 10%;" color="#89a324">
+                    <v-card style="white-space: normal; max-width: 60%;" color="transparent" flat class="white--text">
+                        Project Toevoegen
+                    </v-card>
+                </v-btn>
+                <v-text-field solo hide-details round label="zoek een project" class="search-bar pa-0" flat append-icon="search"></v-text-field>
             </v-layout>
         </v-flex>
     </v-layout>
