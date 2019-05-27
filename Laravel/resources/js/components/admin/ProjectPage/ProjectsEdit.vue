e3f5469c8d1708826118f68ecb5e2fcbb7354aac
Huseyin Caliskan <huseyin.32@live.nl>
2019-05-27 22:17:13 +0200

    Fixed edit page

--- Laravel/resources/js/components/admin/ProjectPage/ProjectsEdit.vue
+++ Laravel/resources/js/components/admin/ProjectPage/ProjectsEdit.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="projectEditSection" v-bar >
-        <div class="pr-2">
+    <div class="projectEditSection" v-bar>
+        <div class="testingCSS">
             <v-layout align-center justify-space-between row>
-                <v-card-title class="display-1">Nieuw Project</v-card-title>
+                <v-card-title class="display-1">Project</v-card-title>
                 <v-btn fab flat @click="">
                     <v-icon x-large color="green"> close</v-icon>
                 </v-btn>
@@ -92,12 +92,69 @@
     }
 </script>
 
-<style scoped>
-.projectEditSection {
-    height: 100%;
-    border-radius: 20px;
-    border-style: solid;
-    border-width: 2px;
-    border-color: #89a226;
-}
+<style>
+    .projectEditSection {
+        height: 100%;
+        border-radius: 20px;
+        border-style: solid;
+        border-width: 2px;
+        border-color: #89a226;
+    }
+
+    .testingCSS::-webkit-scrollbar {
+        display: none;
+    }
+
+    .projectTable .v-datatable .v-datatable__actions .v-datatable__actions__select {
+        display: none;
+    }
+
+    .projectTable .v-table__overflow {
+        overflow-y: hidden;
+        padding-right: 15px;
+    }
+
+    .vb > .vb-dragger {
+        z-index: 5;
+        width: 12px;
+        right: 0;
+    }
+
+    .vb > .vb-dragger > .vb-dragger-styler {
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        -webkit-transform: rotate3d(0, 0, 0, 0);
+        transform: rotate3d(0, 0, 0, 0);
+        -webkit-transition: background-color 100ms ease-out,
+        margin 100ms ease-out,
+        height 100ms ease-out;
+        transition: background-color 100ms ease-out,
+        margin 100ms ease-out,
+        height 100ms ease-out;
+        background-color: rgba(38, 38, 38, 0.1);
+        margin: 5px 5px 5px 0;
+        border-radius: 20px;
+        height: calc(100% - 10px);
+        display: block;
+    }
+
+    .vb.vb-scrolling-phantom > .vb-dragger > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.3);
+    }
+
+    .vb > .vb-dragger:hover > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.5);
+        margin: 0px;
+        height: 100%;
+    }
+
+    .vb.vb-dragging > .vb-dragger > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.5);
+        margin: 0px;
+        height: 100%;
+    }
+
+    .vb.vb-dragging-phantom > .vb-dragger > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.5);
+    }
 </style>
