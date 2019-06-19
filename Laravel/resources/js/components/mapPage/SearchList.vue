79bb7736c40e0a049cfdd619065c8ea2750f3cdd
Huseyin Caliskan <Huseyincaliskan32@gmail.com>
2019-06-19 15:47:15 +0200

    Made mappage searchbar responsive

---
+++ Laravel/resources/js/components/mapPage/SearchList.vue
@@ -0,0 +1,85 @@
+<template>
+    <v-layout fill-height align-start justify-start>
+        <v-list style="background-color: transparent; width: 100%; padding: 0;"
+                class="white--text" two-line
+                pt-0>
+            <template v-for="(item, index) in filteredMapObjects">
+                <v-list-tile
+                    :key="index"
+                    avatar
+                    ripple
+                    style="background-color: rgba(137, 163, 36, 0.9);"
+                    @click="onSearchTileClicked(item)"
+                >
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.name }}</v-list-tile-title>
+                        <v-list-tile-sub-title class="white--text">{{ item.information }}
+                        </v-list-tile-sub-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </template>
+        </v-list>
+    </v-layout>
+</template>
+
+<script>
+    export default {
+        name: "SearchList",
+        props: {
+            filteredMapObjects: {
+                type: Array,
+                required: true,
+            },
+            onSearchTileClicked: {
+                type: Function,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style>
+    .vb > .vb-dragger {
+        z-index: 5;
+        width: 12px;
+        right: 0;
+    }
+
+    .vb > .vb-dragger > .vb-dragger-styler {
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        -webkit-transform: rotate3d(0, 0, 0, 0);
+        transform: rotate3d(0, 0, 0, 0);
+        -webkit-transition: background-color 100ms ease-out,
+        margin 100ms ease-out,
+        height 100ms ease-out;
+        transition: background-color 100ms ease-out,
+        margin 100ms ease-out,
+        height 100ms ease-out;
+        background-color: rgba(38, 38, 38, 0.1);
+        margin: 5px 5px 5px 0;
+        border-radius: 20px;
+        height: calc(100% - 10px);
+        display: block;
+    }
+
+    .vb.vb-scrolling-phantom > .vb-dragger > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.3);
+    }
+
+    .vb > .vb-dragger:hover > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.5);
+        margin: 0px;
+        height: 100%;
+    }
+
+    .vb.vb-dragging > .vb-dragger > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.5);
+        margin: 0px;
+        height: 100%;
+    }
+
+    .vb.vb-dragging-phantom > .vb-dragger > .vb-dragger-styler {
+        background-color: rgba(38, 38, 38, 0.5);
+    }
+</style>
